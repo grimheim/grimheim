9721ff10310c21b23db82a7a1c7957d8669a430c
mikeyhc <mikeyhc@atmosia.net>
2018-04-03 23:13:53 +0200

    added gui test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![deny(missing_docs)]
+
+//! Entry point for Grimheim.
+
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow;
+use opengl_graphics::OpenGL;
+use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::window::WindowSettings;
+
+fn create_window() -> GlutinWindow {
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("Grimheim", [512; 2])
+        .srgb(false)
+        .opengl(opengl)
+        .exit_on_esc(true);
+    settings.build().expect("Could not create window")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = create_window();
+    let mut events = Events::new(EventSettings::new().lazy(true));
+
+    while let Some(_e) = events.next(&mut window) {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn make_window() {
+        create_window();
+    }
 }
